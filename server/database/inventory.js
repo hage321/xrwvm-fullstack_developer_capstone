ebe733881e2cf7901b273666e77bf8f7f1d90e37
hage321 <noreplyinsidious@gmail.com>
2025-10-03 09:42:22 -0400

    second fixed linter

--- server/database/inventory.js
+++ server/database/inventory.js
@@ -1,32 +1,34 @@
+/* jshint esversion: 8 */
+
 const { Int32 } = require('mongodb');
 const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
 const cars = new Schema({
-dealer_id: {
-    type: Number,
-    required: true
-},
-make: {
-    type: String,
-    required: true
+  dealer_id: {
+      type: Number,
+      required: true
   },
-model: {
-    type: String,
-    required: true
+  make: {
+      type: String,
+      required: true
   },
-bodyType: {
-    type: String,
-    required: true
+  model: {
+      type: String,
+      required: true
   },
-year: {
-    type: Number,
-    required: true
+  bodyType: {
+      type: String,
+      required: true
   },
-mileage: {
-    type: Number,
-    required: true
+  year: {
+      type: Number,
+      required: true
+  },
+  mileage: {
+      type: Number,
+      required: true
   }
 });
 
--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,39 +1,30 @@
-# Uncomment the following imports before adding the Model code
-
 from django.db import models
-from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
-# Create your models here.
-
 class CarMake(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=50)
     description = models.TextField()
-    # Add other fields if needed
 
     def __str__(self):
         return self.name
 
 
 class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-one relationship
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
-    
+
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
         ('SUV', 'SUV'),
         ('WAGON', 'Wagon'),
-        # Add more types if needed
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
-    
-    year = models.IntegerField(default=2023,
-        validators=[
-            MaxValueValidator(2023),
-            MinValueValidator(2015)
-        ])
-    # Add other fields if needed
+
+    year = models.IntegerField(
+        default=2023,
+        validators=[MaxValueValidator(2023), MinValueValidator(2015)]
+    )
 
     def __str__(self):
-        return f"{self.car_make} {self.name}"  # Show both make and model
+        return f"{self.car_make} {self.name}"
